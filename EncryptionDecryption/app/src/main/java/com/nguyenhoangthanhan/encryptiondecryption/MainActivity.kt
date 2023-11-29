d81a55550303968fa2b59b511c8e3bc76dbaf766
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-11-30 01:30:35 +0700

    Creating CryptoManager class to generate KeyStore and add encryption and decryption functions

--- EncryptionDecryption/app/src/main/java/com/nguyenhoangthanhan/encryptiondecryption/MainActivity.kt
+++ EncryptionDecryption/app/src/main/java/com/nguyenhoangthanhan/encryptiondecryption/MainActivity.kt
@@ -3,44 +3,64 @@ package com.nguyenhoangthanhan.encryptiondecryption
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nguyenhoangthanhan.encryptiondecryption.ui.theme.EncryptionDecryptionTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val cryptoManager = CryptoManager()
         setContent {
             EncryptionDecryptionTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                var messageToEncrypt by remember {
+                    mutableStateOf("")
                 }
+                var messageToDecrypt by remember {
+                    mutableStateOf("")
+                }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp)
+                ) {
+                    TextField(
+                        value = messageToEncrypt,
+                        onValueChange = {
+                            messageToEncrypt = it
+                        },
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = { Text(text = "Encrypt string")}
+                    )
+                    
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row (
+
+                    ){
+
+                    }
+                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EncryptionDecryptionTheme {
-        Greeting("Android")
     }
 }
